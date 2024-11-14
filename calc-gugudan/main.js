da754c30a70f7a02ae32f5507fe8ebe3caa2c941
lgm-7 <xlzlrud@naver.com>
2024-11-14 15:31:38 +0900

    Update main.js

--- calc-gugudan/main.js
+++ calc-gugudan/main.js
@@ -31,35 +31,65 @@ function calculator() {
     2.뺄셈
     3.곱셈
     4.나눗셈
+    5.돌아가기
   `);
   console.log(`${calchoice}를 선택했습니다.`);
-  switch (choice) {
+  switch (calchoice) {
     case "1":
-      add();
-      break;
+      console.log(add());
+      console.log("\n사칙연산 선택지로 돌아갑니다")
+      calculator();
     case "2":
-      subtract();
-      break;
+      console.log(subtract());
+      console.log("\n사칙연산 선택지로 돌아갑니다")
+      calculator();
     case "3":
-      multiply();
-      break;
+      console.log(multiply());
+      console.log("\n사칙연산 선택지로 돌아갑니다")
+      calculator();
     case "4":
-      divide();
-      break;
+      console.log(divide());
+      console.log("\n사칙연산 선택지로 돌아갑니다")
+      calculator();
+    case "5": main()
+    default:console.log("잘못된 선택입니다 다시 선택해주세요");
+      calculator();
   }
 }
 
-function add(){
-
+function add(a, b) {
+  a = readlineSync.question("덧셈할 첫번째 수를 입력하세요: ");
+  b = readlineSync.question("덧셈할 두번째 수를 입력하세요: ");
+  return Number(a) + Number(b);
 }
 
-function subtract(){
-  
+function subtract(a, b) {
+  a = readlineSync.question("뺄셈할 첫번째 수를 입력하세요: ");
+  b = readlineSync.question("뺄셈할 두번째 수를 입력하세요: ");
+  return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+  a = readlineSync.question("곱셈할 첫번째 수를 입력하세요: ");
+  b = readlineSync.question("곱셈할 두번째 수를 입력하세요: ");
+  return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+  a = readlineSync.question("나눗셈할 첫번째 수를 입력하세요: ");
+  b = readlineSync.question("나눗셈할 두번째 수를 입력하세요: ");
+  return Number(a) / Number(b);
 }
 
 // 구구단 출력 함수
 function printMultiplicationTable() {
   // 여기에 코드를 작성하세요.
+  let a = readlineSync.question("구구단할 단수를 입력하세요: ");
+  for(let i=1; i<10; i++) {
+    console.log(a*i)
+  }
+  console.log("\n처음 선택지로 돌아갑니다")
+  main()
 }
 
 // 프로그램 실행
