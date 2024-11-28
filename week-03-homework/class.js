bbac756a08692e200b649264c9e87d08f64d32c8
lgm-7 <xlzlrud@naver.com>
2024-11-28 12:12:27 +0900

    add

---
+++ week-03-homework/class.js
@@ -0,0 +1,36 @@
+class Character {
+    constructor(name,level,hp,atkpower){
+        this.name = name,
+        this.level = level,
+        this.hp = hp,
+        this.atkpower = atkpower
+    }
+    attack(){
+        console.log(`${this.name}가 공격을 시도합니다!`)
+    }
+    heal(up){
+        console.log(`${this.name}의 체력이 ${up}만큼 회복되었습니다.`)
+    }
+    levelUp(){
+        this.level +=1
+        this.hp +=Math.floor(Math.random()*10)
+        this.atkpower +=Math.floor(Math.random()*10)
+    }
+}
+
+const firstCharacter = new Character("전사",10,300,10)
+const secondCharacter = new Character("궁수",10,200,30)
+const thirdCharacter = new Character("마법사",8,150,30)
+
+console.log(firstCharacter)
+console.log(secondCharacter)
+console.log(thirdCharacter)
+
+firstCharacter.attack()
+firstCharacter.heal(5)
+secondCharacter.attack()
+secondCharacter.heal(5)
+thirdCharacter.attack()
+thirdCharacter.heal(5)
+firstCharacter.levelUp()
+console.log(firstCharacter)
---
+++ week-03-homework/promise.js
@@ -0,0 +1,19 @@
+const findTreasure = new Promise((resolve, reject) => {
+    console.log("보물을 찾는 중입니다.")
+    setTimeout(()=>{
+        const success = Math.random() < 0.1
+        if(success){
+            resolve("축하합니다! 황금 보물을 발견했습니다.")
+        } else {
+            reject("보물을 찾는 데 실패했습니다. 다시 시도하세요.")
+        }
+    },3000)
+})
+
+findTreasure
+  .then((success)=>{
+    console.log(success)
+  })
+  .catch((fail)=>{
+    console.log(fail)
+  })
