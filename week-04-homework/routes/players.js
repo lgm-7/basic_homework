4dbbced0129a7f668acfb67aaca25f469a949158
lgm-7 <xlzlrud@naver.com>
2024-12-09 17:48:18 +0900

    fix

--- week-04-homework/routes/players.js
+++ week-04-homework/routes/players.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+//플레이어 목록
 const players = [
   { name: "차범근", speed: 100, shouting: 100, grade: "s" },
   { name: "메시", speed: 100, shouting: 100, grade: "s" },
@@ -7,6 +8,7 @@ const players = [
 ];
 const router = express.Router();
 
+//플레이어 추가
 router.post("/player", async (req, res) => {
   const { name, speed, shouting, grade } = req.body;
   players.push({
@@ -18,10 +20,12 @@ router.post("/player", async (req, res) => {
   res.status(200).json({ message: "플레이어 추가 완료" });
 });
 
+//플레이어 조회
 router.get("/player", async (req, res) => {
   res.status(200).json({ players });
 });
 
+//플레이어 이름제외 정보수정
 router.put("/player/:index", async (req, res) => {
   const index = req.params.index;
   const { speed, shouting, grade } = req.body;
@@ -39,11 +43,12 @@ router.put("/player/:index", async (req, res) => {
   }
 });
 
+//플레이어 삭제
 router.delete("/player/:index", async (req, res) => {
   const index = req.params.index;
   if (index >= 0 && index < players.length) {
     players.splice(index, 1);
-    res.status(200).json({ message: "플레이가 삭제되었습니다." });
+    res.status(200).json({ message: "플레이어가 삭제되었습니다." });
   } else {
     res.status(404).json({ message: "플레이어가 없습니다." });
   }
