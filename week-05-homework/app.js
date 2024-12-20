8a39b420c1e886f354ee1b2e725915a8c460a9dc
lgm-7 <xlzlrud@naver.com>
2024-12-20 10:08:55 +0900

    Update app.js

--- week-05-homework/app.js
+++ week-05-homework/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import  players  from './routes/players.js';
+import express from "express";
+import players from "./routes/players.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //body데이터를 json으로 변환
 
-app.use('/api/auth', players);
+app.use("/api/auth", players);
 
 app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중입니다.');
+  console.log("서버가 3000번 포트에서 실행 중입니다.");
 });
